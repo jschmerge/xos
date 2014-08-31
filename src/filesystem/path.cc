9f3053b4a433b149d6f92951b790a123d737bbb1
John Schmerge <john.schmerge@gmail.com>
2014-08-31 17:11:28 -0400

    Broke out path class into its own header
    And started implementation in path.cc

---
+++ src/filesystem/path.cc
@@ -0,0 +1,56 @@
+#include "path.h"
+
+namespace std {
+ namespace experimental {
+  namespace filesystem {
+   inline namespace v1 {
+
+path::path() noexcept
+  : pathname(), seperators() 
+	{ }
+
+path::path(const path & other)
+  : pathname(other.pathname), seperators(other.seperators) { }
+
+path::path(path && other) noexcept
+  : pathname(std::move(other.pathname))
+  , seperators(std::move(other.seperators))
+	{ }
+
+path::~path() { }
+
+path & path::operator = (const path & other)
+{
+	if (this != &other)
+	{
+		pathname = other.pathname;
+		seperators = other.seperators;
+	}
+
+	return *this;
+}
+
+path & path::operator = (path && other) noexcept
+{
+	using std::swap;
+	if (this != &other)
+	{
+		swap(pathname, other.pathname);
+		swap(seperators, other.seperators);
+	}
+
+	return *this;
+}
+
+#if XXX
+path & path::operator /= (const path & )
+{
+	return *this;
+}
+#endif
+
+   } /*v1*/
+  }/*filesystem*/
+ } /*experimental*/
+} /*std*/
+
