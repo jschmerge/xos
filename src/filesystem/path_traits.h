2fcc691930fb0bd94c878e0eae03415962aa96e2
John Schmerge <john.schmerge@gmail.com>
2014-09-02 22:43:32 -0400

    old version of path_traits.h that needs committing before merge

--- src/filesystem/path_traits.h
+++ src/filesystem/path_traits.h
@@ -1,36 +1,51 @@
 #ifndef GUARD_PATH_TRAITS_H
 #define GUARD_PATH_TRAITS_H 1
 
-#include <string>
+#include <type_traits>
 
 namespace filesystem {
 inline namespace v1 {
 namespace path_traits {
 
 /// [8.2.2] - encodable path characters
-template <typename C>
-struct is_path_encodable { static constexpr bool value = false; };
+template <typename C> struct is_path_encodable : std::false_type { };
 
-template <>
-struct is_path_encodable<char> { static constexpr bool value = true; };
-
-template <>
-struct is_path_encodable<wchar_t> { static constexpr bool value = true; };
-
-template <>
-struct is_path_encodable<char16_t> { static constexpr bool value = true; };
-
-template <>
-struct is_path_encodable<char32_t> { static constexpr bool value = true; };
+template <> struct is_path_encodable<char> : std::true_type { };
+template <> struct is_path_encodable<wchar_t> : std::true_type { };
+template <> struct is_path_encodable<char16_t> : std::true_type { };
+template <> struct is_path_encodable<char32_t> : std::true_type { };
 
 /// [8.3] - source requirements
-template <typename C, typename T, typename A,
-          template<typename, typename, typename> class STR>
-struct is_path_source
-{
-	static constexpr bool value =
-	  is_path_encodable<typename STR<C, T, A>::value_type>::value;
+
+template <typename T> struct is_path_array_source : std::false_type { };
+
+template <typename T>
+struct is_path_array_source<T*> {
+	typedef typename std::decay<
+		typename std::remove_cv<T>::type>::type value_type;
+	static constexpr bool value = is_path_encodable<value_type>::value;
 };
+
+template <typename T>
+struct is_path_array_source<T[]> {
+	typedef typename std::remove_cv<
+		typename std::decay<T>::type>::type value_type;
+	static constexpr bool value = is_path_encodable<value_type>::value;
+};
+
+template <typename C>
+struct is_path_container_source
+{
+//	typedef typename C::iterator iterator_type;
+
+	typedef typename std::decay<
+		typename std::remove_cv<typename C::value_type>::type>::type value_type;
+	static constexpr bool value = is_path_encodable<value_type>::value;
+};
+
+template <typename T>
+struct is_path_source : std::false_type { };
+
 
 } // namespace path_traits
 } // inline namespace v1
