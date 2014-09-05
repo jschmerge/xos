12f920e8548bcf2a39ebdf2bbe22369140abff51
John Schmerge <john.schmerge@gmail.com>
2014-09-05 08:56:19 -0400

    Adding filesystem_error.{h,cc} into the build

---
+++ src/filesystem/filesystem_error.cc
@@ -0,0 +1,34 @@
+#include "filesystem_error.h"
+
+namespace filesystem {
+inline namespace v1 {
+
+filesystem_error::filesystem_error(const std::string & what_arg,
+                                   std::error_code ec)
+  : std::system_error(ec, what_arg), p1(), p2()
+	{ }
+
+filesystem_error::filesystem_error(const std::string & what_arg,
+                                   const path & p,
+                                   std::error_code ec)
+  : std::system_error(ec, what_arg + " (target = " + p.c_str() + ")")
+  , p1(p), p2()
+	{ }
+
+filesystem_error::filesystem_error(const std::string & what_arg,
+                                   const path & _p1, const path & _p2,
+                                   std::error_code ec)
+  : std::system_error(ec, what_arg +
+                      " (source = " + _p1.c_str() +
+                      ", dest = "+ _p2.c_str() + ")")
+  , p1(_p1), p2(_p2)
+	{ }
+
+const path & filesystem_error::path1() const noexcept
+	{ return p1; }
+
+const path & filesystem_error::path2() const noexcept
+	{ return p2; }
+
+} // inline namespace v1
+} // namespace filesystem
---
+++ src/filesystem/filesystem_error.h
@@ -0,0 +1,36 @@
+#ifndef GUARD_FILESSYSTEM_ERROR_H
+#define GUARD_FILESSYSTEM_ERROR_H 1
+
+#include <memory>
+#include <string>
+#include <system_error>
+#include "path.h"
+
+namespace filesystem {
+inline namespace v1 {
+
+class filesystem_error : public std::system_error
+{
+ public:
+	filesystem_error(const std::string & what_arg, std::error_code ec);
+
+	filesystem_error(const std::string & what_arg, const path & p1,
+	                 std::error_code ec);
+
+	filesystem_error(const std::string & what_arg, const path & p1,
+	                 const path & p2, std::error_code ec);
+
+	const path & path1() const noexcept;
+
+	const path & path2() const noexcept;
+
+ protected:
+	const path p1;
+	const path p2;
+	mutable std::shared_ptr<std::string> xxx;
+};
+
+} // inline namespace v1
+} // namespace filesystem
+
+#endif // GUARD_FILESSYSTEM_ERROR_H
