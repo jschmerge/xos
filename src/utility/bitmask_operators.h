1c2c11018a1479c6b022b6a0f235c88927deae67
John Schmerge <john.schmerge@gmail.com>
2014-10-04 12:37:49 -0400

    Added is_set to list here

--- src/utility/bitmask_operators.h
+++ src/utility/bitmask_operators.h
@@ -2,24 +2,25 @@
 #define GUARD_BITMASK_OPERATORS_H 1
 
 // Macro for defining bitwise operators for enum types
-#define DEFINE_BITMASK_OPERATORS(enum_type, int_type)                \
-	inline constexpr enum_type operator & (enum_type a, enum_type b) \
-	{ return static_cast<enum_type>(                                 \
-		static_cast<int_type>(a) & static_cast<int_type>(b)); }      \
-	inline constexpr enum_type operator | (enum_type a, enum_type b) \
-	{ return static_cast<enum_type>(                                 \
-		static_cast<int_type>(a) | static_cast<int_type>(b)); }      \
-	inline constexpr enum_type operator ^ (enum_type a, enum_type b) \
-	{ return static_cast<enum_type>(                                 \
-		static_cast<int_type>(a) ^ static_cast<int_type>(b)); }      \
-	inline constexpr enum_type operator ~ (enum_type a)              \
-	{ return static_cast<enum_type>(~static_cast<int_type>(a)); }    \
-	inline enum_type & operator &= (enum_type & a, const enum_type & b)    \
-	{ a = (a & b); return a; }                                       \
-	inline enum_type & operator |= (enum_type & a, const enum_type & b)    \
-	{ a = (a | b); return a; }                                       \
-	inline enum_type & operator ^= (enum_type & a, const enum_type & b)    \
-	{ a = (a ^ b); return a; }
-
+#define DEFINE_BITMASK_OPERATORS(enum_type, int_type)                   \
+	inline constexpr enum_type operator & (enum_type a, enum_type b)    \
+	{ return static_cast<enum_type>(                                    \
+		static_cast<int_type>(a) & static_cast<int_type>(b)); }         \
+	inline constexpr enum_type operator | (enum_type a, enum_type b)    \
+	{ return static_cast<enum_type>(                                    \
+		static_cast<int_type>(a) | static_cast<int_type>(b)); }         \
+	inline constexpr enum_type operator ^ (enum_type a, enum_type b)    \
+	{ return static_cast<enum_type>(                                    \
+		static_cast<int_type>(a) ^ static_cast<int_type>(b)); }         \
+	inline constexpr enum_type operator ~ (enum_type a)                 \
+	{ return static_cast<enum_type>(~static_cast<int_type>(a)); }       \
+	inline enum_type & operator &= (enum_type & a, const enum_type & b) \
+	{ a = (a & b); return a; }                                          \
+	inline enum_type & operator |= (enum_type & a, const enum_type & b) \
+	{ a = (a | b); return a; }                                          \
+	inline enum_type & operator ^= (enum_type & a, const enum_type & b) \
+	{ a = (a ^ b); return a; }                                          \
+	inline constexpr bool is_set(enum_type set, enum_type value)        \
+	{ return ((set & value) != enum_type::none); }
 
 #endif // GUARD_BITMASK_OPERATORS_H
