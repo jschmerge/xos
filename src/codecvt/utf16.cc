20d6b05b18890a1a05c1e1bd0b65d48cc71c96a5
John Schmerge <john.schmerge@gmail.com>
2014-10-06 02:33:50 -0400

    Added table from libc++

--- src/codecvt/utf16.cc
+++ src/codecvt/utf16.cc
@@ -4,6 +4,25 @@
 0x10000 - 0x10ffff -> {0xd800 - 0xdbff , 0xdc000 - dffff}
 */
 
+/*
+
+This was taken from the clang libc++ codebase and edited for formatting:
+
+                              Valid UTF ranges
+    UTF-32           UTF-16                 UTF-8               # of cp's
+                  1st      2nd     1st    2nd    3rd    4th
+000000-00007F 0000-007F            00-7F                        127
+000080-0007FF 0080-07FF            C2-DF, 80-BF                 1920
+000800-000FFF 0800-0FFF            E0-E0, A0-BF, 80-BF          2048
+001000-00CFFF 1000-CFFF            E1-EC, 80-BF, 80-BF          49152
+00D000-00D7FF D000-D7FF            ED-ED, 80-9F, 80-BF          2048
+00D800-00DFFF             invalid
+00E000-00FFFF E000-FFFF            EE-EF, 80-BF, 80-BF          8192
+010000-03FFFF D800-D8BF, DC00-DFFF F0-F0, 90-BF, 80-BF, 80-BF   196608
+040000-0FFFFF D8C0-DBBF, DC00-DFFF F1-F3, 80-BF, 80-BF, 80-BF   786432
+100000-10FFFF DBC0-DBFF, DC00-DFFF F4-F4, 80-8F, 80-BF, 80-BF   65536
+*/
+
 #include <utility>
 #include <stdexcept>
 
