dd70b8b469fb07451e5fb8cca51af7c932794b32
John Schmerge <john.schmerge@gmail.com>
2014-10-05 21:00:13 -0400

    Test code

---
+++ src/codecvt/test.cc
@@ -0,0 +1,37 @@
+#include "utf8.h"
+
+class u32cvt : public std::codecvt<char32_t, char, std::mbstate_t>
+{
+ public:
+	using codecvt<char32_t, char, std::mbstate_t>::codecvt;
+	virtual ~u32cvt() { };
+};
+
+
+int main()
+{
+	u32cvt cvt;
+	const char32_t * doneptr = nullptr;;
+
+	char outbuf[64];
+
+	char * end = outbuf + 64, * ptr2 = nullptr;
+
+	auto state = std::mbstate_t();
+	for (char32_t i = 0; i < 0x10ffff; ++i)
+	{
+		auto res = cvt.out(state, &i, (&i) + 1, doneptr, outbuf, end, ptr2);
+		if ((i % 0x10000) == 0 || ((i < 0x10000) && ((i % 0x100) == 0)))
+			printf("processing plane 0x%08x, current len = %ld\n",
+			       i, ptr2 - outbuf);
+
+		assert(res == std::codecvt_base::ok);
+
+		size_t length = ptr2 - outbuf;
+
+		state = std::mbstate_t();
+		auto l2 = cvt.length(state, outbuf, ptr2, 20);
+
+		printf("Length %d %zu\n", l2, length);
+	}
+}
