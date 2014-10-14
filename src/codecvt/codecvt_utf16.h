c587e0de9e6c9c5f91c84ce55bb77d132853c78d
John Schmerge <john.schmerge@gmail.com>
2014-10-14 18:40:57 -0400

    More flesh to this now

--- src/codecvt/codecvt_utf16.h
+++ src/codecvt/codecvt_utf16.h
@@ -24,8 +24,58 @@ template <typename Elem, unsigned long Maxcode = 0x10ffff,
 class codecvt_utf16 : public codecvt<Elem, char, mbstate_t>
 {
  public:
-	explicit codecvt_utf16(size_t refs = 0);
-	~codecvt_utf16();
+	typedef Elem      intern_type;
+	typedef char      extern_type;
+	typedef mbstate_t state_type;
+
+	explicit codecvt_utf16(size_t refs = 0)
+	: codecvt<intern_type, extern_type, mbstate_t>(refs)
+	{ }
+
+	~codecvt_utf16()
+	{ }
+
+ protected:
+	virtual codecvt_base::result
+	do_out(mbstate_t & state,
+	       const intern_type * from_begin,
+	       const intern_type * from_end,
+	       const intern_type * & from_last,
+	       char * to_begin,
+	       char * to_end,
+	       char * & to_last) const override;
+
+	virtual codecvt_base::result
+	do_unshift(mbstate_t & state,
+	           char * to_begin,
+	           char * to_end,
+	           char * & to_last) const override;
+
+	virtual codecvt_base::result
+	do_in(mbstate_t & state,
+	      const char * from_begin,
+	      const char * from_end,
+	      const char * & from_last,
+	      intern_type * to_begin,
+	      intern_type * to_end,
+	      intern_type * & to_last) const override;
+
+	virtual int
+	do_length(mbstate_t & state,
+	          const char * from_begin,
+	          const char * from_end,
+	          size_t) const override;
+
+	virtual int
+	do_encoding() const noexcept override
+	{ return 0; }
+
+	virtual bool
+	do_always_noconv() const noexcept override
+	{ return false; }
+
+	virtual int
+	do_max_length() const noexcept override;
 };
 
 } // namespace std
