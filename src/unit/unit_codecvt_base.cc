87e53b620a97c09b0b93248a596b1b1246dc3cd7
John Schmerge <john.schmerge@gmail.com>
2014-12-27 21:56:31 -0500

    Added all of the codecvt_utfxxxx classes (without all permutations of template params)

--- src/unit/unit_codecvt_base.cc
+++ src/unit/unit_codecvt_base.cc
@@ -69,12 +69,26 @@ class Test_codecvt_base : public CppUnit::TestFixture
 	}
 };
 
-typedef Test_codecvt_base<std::codecvt<char16_t, char, std::mbstate_t>> a;
-typedef Test_codecvt_base<std::codecvt<char32_t, char, std::mbstate_t>> b;
-typedef Test_codecvt_base<std::codecvt_utf8<char16_t, 0xff, std::codecvt_mode(0)>> c;
-typedef Test_codecvt_base<std::codecvt_utf8<char32_t>> d;
+typedef Test_codecvt_base<std::codecvt<char16_t, char, std::mbstate_t>> c16;
+typedef Test_codecvt_base<std::codecvt<char32_t, char, std::mbstate_t>> c32;
 
-CPPUNIT_TEST_SUITE_REGISTRATION(a);
-CPPUNIT_TEST_SUITE_REGISTRATION(b);
-CPPUNIT_TEST_SUITE_REGISTRATION(c);
-CPPUNIT_TEST_SUITE_REGISTRATION(d);
+typedef Test_codecvt_base<std::codecvt_utf8<char16_t>> c_utf8_16;
+typedef Test_codecvt_base<std::codecvt_utf8<char32_t>> c_utf8_32;
+
+typedef Test_codecvt_base<std::codecvt_utf16<char16_t>> c_utf16_16;
+typedef Test_codecvt_base<std::codecvt_utf16<char32_t>> c_utf16_32;
+
+typedef Test_codecvt_base<std::codecvt_utf8_utf16<char16_t>> c_utf8_utf16_16;
+typedef Test_codecvt_base<std::codecvt_utf8_utf16<char32_t>> c_utf8_utf16_32;
+
+CPPUNIT_TEST_SUITE_REGISTRATION(c16);
+CPPUNIT_TEST_SUITE_REGISTRATION(c32);
+
+CPPUNIT_TEST_SUITE_REGISTRATION(c_utf8_16);
+CPPUNIT_TEST_SUITE_REGISTRATION(c_utf8_32);
+
+CPPUNIT_TEST_SUITE_REGISTRATION(c_utf16_16);
+CPPUNIT_TEST_SUITE_REGISTRATION(c_utf16_32);
+
+CPPUNIT_TEST_SUITE_REGISTRATION(c_utf8_utf16_16);
+CPPUNIT_TEST_SUITE_REGISTRATION(c_utf8_utf16_32);
