0fa89b10616a0bfc2be1913def105579463721e6
John Schmerge <john.schmerge@gmail.com>
2015-01-02 07:52:27 -0500

    Added comparison operators for state

--- src/automata/automaton.h
+++ src/automata/automaton.h
@@ -1,9 +1,9 @@
-#ifndef GUARD_AUTOMATA_H
-#define GUARD_AUTOMATA_H 1
+#ifndef GUARD_AUTOMATON_H
+#define GUARD_AUTOMATON_H 1
 
-#include <string>
 #include <limits>
 #include <vector>
+#include <set>
 
 //////////////////////////////////////////////////////////////////////
 template <typename S_ID_T>
@@ -12,7 +12,7 @@ class state
  public:
 	typedef S_ID_T state_id_type;
 
-	state(const std::string & name) : m_id(name) { }
+	state(const state_id_type & name) : m_id(name) { }
 
 	state(const state & other) : m_id(other.m_id) { }
 
@@ -32,11 +32,40 @@ class state
 		return *this;
 	}
 
-	state_id_type id() const { return m_id; }
+	state_id_type id() const
+	{
+		return m_id;
+	}
 
  protected:
 	state_id_type m_id;
 };
+
+//////////////////////////////////////////////////////////////////////
+template <typename SID_T>
+bool operator == (const state<SID_T> & a, const state<SID_T> & b)
+	{ return a.id() == b.id(); }
+
+template <typename SID_T>
+bool operator != (const state<SID_T> & a, const state<SID_T> & b)
+	{ return a.id() != b.id(); }
+
+template <typename SID_T>
+bool operator < (const state<SID_T> & a, const state<SID_T> & b)
+	{ return a.id() < b.id(); }
+
+template <typename SID_T>
+bool operator <= (const state<SID_T> & a, const state<SID_T> & b)
+	{ return a.id() <= b.id(); }
+
+template <typename SID_T>
+bool operator > (const state<SID_T> & a, const state<SID_T> & b)
+	{ return a.id() > b.id(); }
+
+template <typename SID_T>
+bool operator >= (const state<SID_T> & a, const state<SID_T> & b)
+	{ return a.id() >= b.id(); }
+
 
 //////////////////////////////////////////////////////////////////////
 template <typename IN_T, typename S_ID_T>
@@ -96,25 +125,27 @@ class automaton
 
 	void declare_start_state(const state_id_type & id)
 	{
-		using std::swap;
 		declare_state(id);
-		swap(m_states.back(), m_states.front());
 	}
 
 	void declare_state(const state_id_type & id)
-	{ m_states.emplace_back(id); }
+	{
+		m_states.emplace_back(id);
+	}
 
 	void declare_transition(const state_id_type & old_state,
 	                        const state_id_type & new_state,
 	                        const input_type & value)
-	{ m_transitions.emplace_back(old_state, new_state, value); }
+	{
+		m_transitions.emplace_back(old_state, new_state, value);
+	}
 
  protected:
 	void compile() { }
 
  private:
-	std::vector<state<state_id_type>> m_states;
+	std::set<state<state_id_type>> m_states;
 	std::vector<transition<state_id_type, input_type>> m_transitions;
 };
 
-#endif // GUARD_AUTOMATA_H
+#endif // GUARD_AUTOMATON_H
