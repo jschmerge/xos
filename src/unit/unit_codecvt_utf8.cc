dedb76e4f730c01410164438cffebdc1eb18b8a4
John Schmerge <john.schmerge@gmail.com>
2015-01-11 16:05:56 -0500

    Better tests

--- src/unit/unit_codecvt_utf8.cc
+++ src/unit/unit_codecvt_utf8.cc
@@ -4,22 +4,15 @@
 //
 // Seperate impl of this calculation here for the sake of testing
 //
-static int utf8_chars_needed(unsigned long char_value)
+static constexpr int utf8_chars_needed(unsigned long char_value)
 {
-	int ret = -1;
-	if (char_value < 0x80)
-		ret = 1;
-	else if (char_value < 0x800)
-		ret = 2;
-	else if (char_value < 0x10000)
-		ret = 3;
-	else if (char_value < 0x200000)
-		ret = 4;
-	else if (char_value < 0x4000000)
-		ret = 5;
-	else if (char_value < 0x80000000)
-		ret = 6;
-	return ret;
+	return ( (char_value < 0x80) ? 1 :
+	       ( (char_value < 0x800) ?  2 :
+	       ( (char_value < 0x10000) ?  3 :
+	       ( (char_value < 0x200000) ?  4 :
+	       ( (char_value < 0x4000000) ?  5 :
+	       ( (char_value < 0x80000000) ?  6 :
+	         -1 ) ) ) ) ) );
 }
 
 namespace {
@@ -27,6 +20,8 @@ namespace {
   template<class T> constexpr const T& min(const T & a, const T & b )
     { return ( (a < b) ? a : b ); }
 }
+
+static constexpr int bom_length = utf8_chars_needed(bom_value());
 
 template <typename C, unsigned long N, int M>
 class Test_codecvt_utf8
@@ -49,12 +44,18 @@ class Test_codecvt_utf8
 	CPPUNIT_TEST(unshift_errors);
 	CPPUNIT_TEST_SUITE_END();
 
-	static const std::codecvt_mode cvtMode;
-	static const bool has_bom;
-	static const bool eats_bom;
-//	static constexpr unsigned long max_value;
+	static constexpr std::codecvt_mode cvtMode =
+		static_cast<std::codecvt_mode>(M);
+
+	static constexpr bool has_bom = (cvtMode & std::generate_header);
+
+	static constexpr bool eats_bom = (cvtMode & std::consume_header);
+
 	static constexpr unsigned long max_value =
-	  min(N, static_cast<unsigned long>(std::numeric_limits<C>::max()));
+		min(N, static_cast<unsigned long>(std::numeric_limits<C>::max()));
+
+	static constexpr unsigned long max_value_length =
+		utf8_chars_needed(max_value);
 
  public:
 	virtual ~Test_codecvt_utf8() { }
@@ -64,12 +65,12 @@ class Test_codecvt_utf8
 		typename base::cvt_t cvt;
 		if (cvtMode & std::consume_header)
 		{
-			CPPUNIT_ASSERT(
-			  (utf8_chars_needed(max_value) +
-			   utf8_chars_needed(bom_value()) == cvt.max_length()));
+			CPPUNIT_ASSERT( (max_value_length + bom_length) ==
+			                  static_cast<unsigned long>(cvt.max_length()));
 		} else
 		{
-			CPPUNIT_ASSERT(utf8_chars_needed(max_value) == cvt.max_length());
+			CPPUNIT_ASSERT(max_value_length ==
+			                 static_cast<unsigned long>(cvt.max_length()));
 		}
 	}
 
@@ -85,20 +86,15 @@ class Test_codecvt_utf8
 		unsigned long cval = 0;
 		for (; cval <= std::min(0xd7fful, max_value); ++cval)
 		{
-//			printf("\n\t-> Converting %02lX", cval);
 			end = outbuffer + obufsz;
 			rc = out_for_ictype(cval, outbuffer, end);
-
-			CPPUNIT_ASSERT( (rc == std::codecvt_base::ok)
-			  || ( (N < static_cast<unsigned long>(bom_value()))
-			     && (cvtMode & std::generate_header)
-			     && (rc == std::codecvt_base::error)) );
 
 			ictype x = 0;
 			const char * end2 = end;
 			in_for_ictype(x, outbuffer, end2);
+
+			CPPUNIT_ASSERT(static_cast<unsigned long>(x) == cval);
 		}
-		printf("\n");
 
 		for (cval = 0xe000ul;
 		     cval <= std::min(0x10fffful, max_value);
@@ -174,23 +170,19 @@ class Test_codecvt_utf8
 
 		CPPUNIT_ASSERT(rc == std::codecvt_base::ok);
 
-		const int bom_length = utf8_chars_needed(bom_value());
 		const int encoded_length = has_bom ?
 		                             utf8_chars_needed(c) + bom_length :
 		                             utf8_chars_needed(c);
 
-		if( (to_next - buffer) != encoded_length)
-		{
-			printf("\n%08X:, difference = %zd, computed = %d\n",
-			       c, to_next - buffer, encoded_length);
-		}
 		CPPUNIT_ASSERT((to_next - buffer) == encoded_length);
 
 		state = std::mbstate_t();
 		int len = cvt.length(state, buffer, to_next, 4);
 
 		CPPUNIT_ASSERT(  len == encoded_length
-		              || (has_bom && !eats_bom && N < 0xfffful && len == 3) );
+		              || (  has_bom
+		                 && !eats_bom && N < 0xfffful
+		                 && len == bom_length) );
 
 		buffer_end = to_next;
 
@@ -208,49 +200,39 @@ class Test_codecvt_utf8
 		ictype * last = nullptr;
 		const char * saved_end = end;
 
-//		for (auto p = buffer; p < end; ++p)
-//			printf("%02hhX ", *p);
+		if ( (cvtMode & std::generate_header)
+		   && ! (cvtMode & std::consume_header) )
+			buffer += bom_length;
 
 		c = 0;
 		rc = cvt.in(state, buffer, saved_end, end,
 		            conv_buffer, conv_buffer + 10, last);
 
-//		if (rc) printf("\nRC = %d, yeild = %zd\n", rc, last-conv_buffer);
-//		CPPUNIT_ASSERT(rc == std::codecvt_base::ok);
-#if 0
-		if( (has_bom  &&  eats_bom && ((last - conv_buffer) == 1))
-		  || (!has_bom &&  eats_bom && ((last - conv_buffer) == 1))
-		  || (has_bom  && !eats_bom && ((last - conv_buffer) == 2))
-		  || (!has_bom && !eats_bom && ((last - conv_buffer) == 1))
-		  )
-		{ } else {
-			printf("\n");
-			for (auto p = buffer; p < saved_end; ++p)
-				printf("%02hhX ", *p);
-			printf("\nhas %d eats %d\n", has_bom, eats_bom);
-			printf("num eaten = %zd\n", last - conv_buffer);
-			CPPUNIT_ASSERT(false);
-		}
-#endif
+		CPPUNIT_ASSERT(rc == std::codecvt_base::ok);
+		c = conv_buffer[0];
 		              
 		return rc;
 	}
 };
 
+//
+// These are static constexpr variables, unfortunately the compiler isn't
+// smart enough to find a place to generate them on its own
+//
 template <typename C, unsigned long N, int M>
-const std::codecvt_mode Test_codecvt_utf8<C, N, M>::cvtMode =
-	static_cast<std::codecvt_mode>(M);
+  constexpr std::codecvt_mode Test_codecvt_utf8<C, N, M>::cvtMode;
 
 template <typename C, unsigned long N, int M>
-const bool Test_codecvt_utf8<C, N, M>::has_bom =
-	(cvtMode & std::generate_header);
+  constexpr bool Test_codecvt_utf8<C, N, M>::has_bom;
 
 template <typename C, unsigned long N, int M>
-const bool Test_codecvt_utf8<C, N, M>::eats_bom =
-	(cvtMode & std::consume_header);
+  constexpr bool Test_codecvt_utf8<C, N, M>::eats_bom;
 
 template <typename C, unsigned long N, int M>
-constexpr unsigned long Test_codecvt_utf8<C, N, M>::max_value;
+  constexpr unsigned long Test_codecvt_utf8<C, N, M>::max_value;
+
+template <typename C, unsigned long N, int M>
+  constexpr unsigned long Test_codecvt_utf8<C, N, M>::max_value_length;
 
 FOR_ALL_CVT_MODES(REGISTER_UTF_TEST, Test_codecvt_utf8, wchar_t, 0x7f);
 FOR_ALL_CVT_MODES(REGISTER_UTF_TEST, Test_codecvt_utf8, wchar_t, 0xff);
